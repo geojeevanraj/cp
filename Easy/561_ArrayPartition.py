39ee3207cd79d9bcd475daf9d459ec4c7c84eaeb
Geo Jeevan Raj C <125670051+geojeevanraj@users.noreply.github.com>
2026-02-18 23:22:36 +0530

    Add solution for LeetCode Array Partition problem

---
+++ Easy/561_ArrayPartition.py
@@ -0,0 +1,9 @@
+#lc problem link: https://leetcode.com/problems/array-partition
+
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        res = 0
+        for i in range(0,len(nums),2):
+            res += nums[i]
+        return res
